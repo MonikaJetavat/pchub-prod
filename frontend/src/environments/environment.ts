365d26c6d6246c6206dd6837ceda06c4c828c2c3
Monika Chauhan <mchauhan@asite.com>
2026-05-22 19:26:07 +0530

    done

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // environment.ts (development)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://pchub-prod-w7ao.vercel.app/api',
   appName: 'PC HUB 2.0',
   version: '2.0.0',
 };
